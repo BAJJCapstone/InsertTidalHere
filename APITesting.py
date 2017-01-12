6103fe80479ddba3d27a9e7e535e3de4a49d4a16
JASON CAIN <cain.ja@husky.neu.edu>
2017-01-12 17:43:21 -0500

    lots of stuff

--- APITesting.py
+++ APITesting.py
@@ -1,44 +1,131 @@
 
 # coding: utf-8
 
-# In[ ]:
+# In[1]:
 
 import requests
 import json
 from pandas.io.json import json_normalize
+import pandas as pd
 
 with open('token.json') as token_file:
     header = json.load(token_file)
-    print(header)
 
 
-# In[19]:
+# In[36]:
 
-url = 'http://www.ncdc.noaa.gov/cdo-web/api/v2/datacategories?limit=41'
-resp = requests.get(url=url, headers=header)
-print(resp.json())
+pd_stations = pd.read_json('station_info.json')
+pd_stations
+
+
+# In[34]:
+
+import re
+from time import strptime
+import datetime
+
+def datesToInt(date):
+    
+    if date = 'present':
+        date_string = time.strftime("%x")
+        now = datetime.datetime.now()
+        return now.year, now.month, now.day
+    date_list = re.findall(r"[\w']+", date)
+    day = int(date_list[1])
+    year = int(date_list[2])
+    month = int(strptime(date_list[0],'%b').tm_mon)
+    
+    return year, month, day
+
+
+# In[37]:
+
+import datetime
+now = datetime.datetime.now()
+type(now.year)
+
+
+# In[40]:
+
+
+def retrieveLifetimeData(pandas_thing):
+    date_range_string = pandas_thing['date']
+    station_id = pandas_thing['ID']
+
+    dates = date_range_string.split('-')
+    assert dates[0] != ''
+    year, month, day = datesToInt(dates[0])
+
+    end_year, end_month, end_day = datesToInt(dates[1])
+
+    pd_metadata = json_normalize(resp.json()['metadata'])
+    pd_metadata.columns = ['Station ID', 'Latitude', 'Longitude', 'Location']
+    pd_data = json_normalize(resp.json()['data'])
+    first_loop = True
+    while 1:
+        end_loop = False
+        if year < end_year and month+1 < end_month and day < end_day:
+            url = 'https://tidesandcurrents.noaa.gov/api/datagetter?'
+            params = {
+                'begin_date': '{0}/{1}/{2}'.format(month, day, year),
+                'end_date':'{0}/{1}/{2}'.format(month+1, day, year),
+                'station':station_id,
+                'product':'water_level',
+                'datum':'mllw',
+                'units':'metric',
+                'time_zone':'gmt',
+                'application':'web_services',
+                'format':'json'    
+            }
+        else:
+            url = 'https://tidesandcurrents.noaa.gov/api/datagetter?'
+            params = {
+                'begin_date': '{0}/{1}/{2}'.format(month, day, year),
+                'end_date':'{0}/{1}/{2}'.format(end_month, end_day, end_year),
+                'station':station_id,
+                'product':'water_level',
+                'datum':'mllw',
+                'units':'metric',
+                'time_zone':'gmt',
+                'application':'web_services',
+                'format':'json'    
+            }            
+            end_loop = True
+        resp = requests.get(url=url, params=params)
+        if first_loop:
+            pd_metadata.append(resp.json()['metadata'])
+            first_loop = False
+        pd_data.append(resp.json()['data'])
+        
+        month += 1
+        if end_loop:
+            return pd_metadata, pd_data
+
+
+# In[46]:
+
+pd_item = pd_stations[0,:]
+retrieveLifetimeData(pd_item)
+
+
+# In[30]:
 
 try:
     pd_metadata = json_normalize(resp.json()['metadata'])
-    # pd_metadata.columns = ['Station ID', 'Latitude', 'Longitude', 'Location']
-    pd_results = json_normalize(resp.json()['results'])
+    pd_metadata.columns = ['Station ID', 'Latitude', 'Longitude', 'Location']
+    pd_data = json_normalize(resp.json()['data'])
     
 except KeyError:
     print(resp.json()['error'])
-    print('Error in the api input')
 
 
-# In[20]:
+# In[31]:
 
-pd_results
-
-
-# In[26]:
+# API for Climate Data
 
 url = 'http://www.ncdc.noaa.gov/cdo-web/api/v2/datatypes?datacategoryid=HYDROMETEOR'
 
 resp = requests.get(url=url, headers=header)
-print(resp.json())
 
 try:
     pd_metadata = json_normalize(resp.json()['metadata'])
@@ -50,56 +137,17 @@ except KeyError:
     print('Error in the api input')
 
 
-# In[27]:
+# In[32]:
 
-pd_results
-
-
-# In[28]:
-
-# Old version of NOAA API
-
-url = 'https://tidesandcurrents.noaa.gov/api/datagetter?'
-
-params = {
-    'begin_date':'20160101 10:00',
-    'end_date':'20160201 10:00',
-    'station':'nb0301',
-    'product':'currents',
-     #'datum':'mllw',
-    'units':'metric',
-    'time_zone':'gmt',
-    'application':'web_services',
-    'format':'json'    
-}
-
-resp = requests.get(url=url, params=params)
-print(resp.json())
-
-
-# In[29]:
+url = 'http://www.ncdc.noaa.gov/cdo-web/api/v2/datacategories?limit=41'
+resp = requests.get(url=url, headers=header)
 
 try:
     pd_metadata = json_normalize(resp.json()['metadata'])
-    pd_metadata.columns = ['Station ID', 'Latitude', 'Longitude', 'Location']
-    pd_data = json_normalize(resp.json()['data'])
+    # pd_metadata.columns = ['Station ID', 'Latitude', 'Longitude', 'Location']
+    pd_results = json_normalize(resp.json()['results'])
     
 except KeyError:
     print(resp.json()['error'])
     print('Error in the api input')
-
-
-# In[36]:
-
-pd_metadata['Station ID'].value_counts()
-
-
-# In[30]:
-
-pd_data
-
-
-# In[ ]:
-
-
 
--- StationIDScraper.py
+++ StationIDScraper.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import urllib.request
+import json
 
 station_url = 'https://tidesandcurrents.noaa.gov/stations.html?type=Historic+Water+Levels'
 with urllib.request.urlopen(station_url) as url:
@@ -10,11 +11,14 @@ soup = BeautifulSoup(station_html, "html.parser")
 stations = soup.find_all('div', {'class': lambda L: L and L.startswith('span4 station')})
 
 station_info = {}
-for station in stations:
-    station_info[station.a.get_text().split()[0]] = {}
-    station_info[station.a.get_text().split()[0]]['dates'] = station.span.get_text()
 
-import json
+station_info['ID'] = {}
+station_info['dates'] = {}
+
+for station in stations:
+    key = ''.join(station.a.get_text().split()[1:])
+    station_info['dates'][key] = station.span.get_text()
+    station_info['ID'][key] = station.a.get_text().split()[0]
 
 with open("station_info.json", "w") as writeJSON:
     json.dump(station_info, writeJSON)
