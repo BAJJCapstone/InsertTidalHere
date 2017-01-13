c710048b80270ae7d0d2ad091c800212b1634cd8
JASON CAIN <cain.ja@husky.neu.edu>
2017-01-12 22:06:30 -0500

    making progress...

--- APITesting.py
+++ APITesting.py
@@ -18,15 +18,14 @@ pd_stations = pd.read_json('station_info.json')
 pd_stations
 
 
-# In[34]:
+# In[58]:
 
 import re
 from time import strptime
 import datetime
 
 def datesToInt(date):
-    
-    if date = 'present':
+    if date == 'present':
         date_string = time.strftime("%x")
         now = datetime.datetime.now()
         return now.year, now.month, now.day
@@ -38,6 +37,17 @@ def datesToInt(date):
     return year, month, day
 
 
+# In[52]:
+
+i = 0
+for index, row in pd_stations.iterrows():
+    print(index)
+    print(row)
+    i += 1
+    if i > 20:
+        break
+
+
 # In[37]:
 
 import datetime
@@ -45,11 +55,11 @@ now = datetime.datetime.now()
 type(now.year)
 
 
-# In[40]:
+# In[76]:
 
 
 def retrieveLifetimeData(pandas_thing):
-    date_range_string = pandas_thing['date']
+    date_range_string = pandas_thing['dates']
     station_id = pandas_thing['ID']
 
     dates = date_range_string.split('-')
@@ -57,18 +67,18 @@ def retrieveLifetimeData(pandas_thing):
     year, month, day = datesToInt(dates[0])
 
     end_year, end_month, end_day = datesToInt(dates[1])
-
-    pd_metadata = json_normalize(resp.json()['metadata'])
-    pd_metadata.columns = ['Station ID', 'Latitude', 'Longitude', 'Location']
-    pd_data = json_normalize(resp.json()['data'])
+    this_is_silly = end_year*10000+end_month*100+end_day
+#     pd_metadata = json_normalize(resp.json()['metadata'])
+#     pd_metadata.columns = ['Station ID', 'Latitude', 'Longitude', 'Location']
+#     pd_data = json_normalize(resp.json()['data'])
     first_loop = True
     while 1:
         end_loop = False
-        if year < end_year and month+1 < end_month and day < end_day:
+        if year*10000+(month+1)*100+end_day < this_is_silly:
             url = 'https://tidesandcurrents.noaa.gov/api/datagetter?'
             params = {
-                'begin_date': '{0}/{1}/{2}'.format(month, day, year),
-                'end_date':'{0}/{1}/{2}'.format(month+1, day, year),
+                'begin_date': '{:02d}/{:02d}/{}'.format(month, day, year),
+                'end_date':'{:02d}/{:02d}/{}'.format(month+1, day, year),
                 'station':station_id,
                 'product':'water_level',
                 'datum':'mllw',
@@ -80,8 +90,8 @@ def retrieveLifetimeData(pandas_thing):
         else:
             url = 'https://tidesandcurrents.noaa.gov/api/datagetter?'
             params = {
-                'begin_date': '{0}/{1}/{2}'.format(month, day, year),
-                'end_date':'{0}/{1}/{2}'.format(end_month, end_day, end_year),
+                'begin_date': '{:02d}/{:02d}/{}'.format(month, day, year),
+                'end_date':'{:02d}/{:02d}/{}'.format(end_month, end_day, end_year),
                 'station':station_id,
                 'product':'water_level',
                 'datum':'mllw',
@@ -93,19 +103,28 @@ def retrieveLifetimeData(pandas_thing):
             end_loop = True
         resp = requests.get(url=url, params=params)
         if first_loop:
-            pd_metadata.append(resp.json()['metadata'])
-            first_loop = False
+            try:
+                
+                metadata_dict = resp.json()['metadata']
+                pd_data = pd.DataFrame(resp.json()['data'])
+            except KeyError:
+                print(resp.json()['error'])
+                return None, None
+            
         pd_data.append(resp.json()['data'])
         
         month += 1
         if end_loop:
-            return pd_metadata, pd_data
+            return metadata_dict, pd_data
 
 
-# In[46]:
+# In[77]:
 
-pd_item = pd_stations[0,:]
-retrieveLifetimeData(pd_item)
+for index, row in pd_stations.iterrows():
+    metadata_dict, pd_data = retrieveLifetimeData(row)
+    if pd_metadata is not None:
+        break
+pd_data
 
 
 # In[30]:
