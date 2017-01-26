0c57bd574a5d0e27d12c3c6f2e42775cf27eb71f
JASON CAIN <cain.ja@husky.neu.edu>
2017-01-26 17:38:33 -0500

    had to fix dates again

--- APITesting.py
+++ APITesting.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[10]:
 
 import requests
 import json
@@ -16,230 +16,87 @@ import datetime
 
 # ### Investigating currents 
 
-# In[2]:
+# In[11]:
 
 with open('current_station_info.json', 'r') as station_info_file:
     currents_station_info = json.load(station_info_file)
     
 with open('current_station_intervals.json', 'r') as station_dates_file:
     currents_station_intervals = json.load(station_dates_file)
+    
+    
 
 
-# In[3]:
-
-def datesToInt(date):
-    if date == '':
-        date_string = time.strftime("%x")
-        now = datetime.datetime.now()
-        return '{}{:02d}{:02d} 00:00:00'.format(now.year, now.month, now.day) 
-    split_date = re.findall(r"[\w']+", date)
-    print(split_date)
-    day = int(split_date[1])
-    year = int(split_date[2])
-    month = int(time.strptime(split_date[0],'%b').tm_mon)
-    time = ':'.join(split_date[-3:])
-    return '{}{:02d}{:02d} {}'.format(year, month, day, time)
-
-
-# In[4]:
+# In[12]:
 
 for key, date_list in currents_station_info.items():
-    print('***********Starting with**************: \n {} \n'.format(date_list))
     if any(isinstance(el, list) for el in date_list):
-        for i, date in enumerate(date_list):
-            print('1.{}'.format(date))
-            currents_station_info[key][i] = [element.replace('-','') for element in date]
-            print('2.{}'.format(currents_station_info[key][i]))
-            if date[1] == '':
-                date_string = time.strftime("%x")
-                now = datetime.datetime.now()
-                currents_station_info[key][0][1] = '{}{:02d}{:02d} 00:00:00'.format(now.year, now.month, now.day)      
-                
+        for i, dates in enumerate(date_list):
+            for j, date in enumerate(dates[:1]):
+                if date[1] == '':
+                    currents_station_info[key][0][1] = datetime.datetime.now()
+                    continue
+                split_date = re.findall(r"[\w']+", date)
+                currents_station_info[key][i][j] = datetime.datetime(year = int(split_date[0]), 
+                                month = int(split_date[1]),
+                                day = int(split_date[2]), 
+                                hour = int(split_date[3]), 
+                                minute = int(split_date[4]),
+                                second = int(split_date[5]))   
     else:
+        for_consistency = []
         tmp_list = []
         for date in date_list[:1]:
-            tmp_list.append(datesToInt(date))
+            split_date = re.findall(r"[\w']+", date)
+            tmp_list.append(datetime.datetime(year = int(split_date[2]), 
+                            month = int(strptime(split_date[0],'%b').tm_mon),
+                            day = int(split_date[1]), 
+                            hour = int(split_date[3]),
+                            minute = int(split_date[4]),
+                            second = int(split_date[5])))
+        
         tmp_list = tmp_list + date_list[2:]
-        currents_station_info[key] = tmp_list
-    
-    print('New Dictionary Entry:{}'.format(currents_station_info[key]))
-    print('\n\n\n ***NEXT*** \n\n\n')
+        currents_station_info[key] = for_consistency.append(tmp_list) #put the list inside of another list
 
 
 # In[ ]:
 
-def retrieveLifetimeData(pandas_thing):
-    date_range_string = pandas_thing['dates']
-    station_id = pandas_thing['ID']
+def retrieveLifetimeData(station_id, date_lists):
+    for date_list in date_lists:
+        begin_date = date_list[0]
+        end_date = date_list[1]
+        date = begin_date
+        month = datetime.timedelta(month=1)
+        first_loop = True
+        while 1:
+            end_loop = False
+            next_date = date + month
+            url = 'https://tidesandcurrents.noaa.gov/api/datagetter?'
+            params = {
+                'begin_date': '{:02d}/{:02d}/{} {:02d}:{:02d}:{:02d}'.format(date.month, date.day, date.year, date.hour, date.minute, date.second),
+                'end_date':'{:02d}/{:02d}/{} {:02d}:{:02d}:{:02d}'.format(next_date.month, next_date.day, next_date.year, next_date.hour, next_date.minute, next_date.second),
+                'station':station_id,
+                'product':'currents',
+                'units':'metric',
+                'time_zone':'gmt',
+                'application':'web_services',
+                'format':'json'    
+            }
+            for i in range(1,10):
+                print(i)
+                params['bin'] = i
+                resp = requests.get(url=url, params=params)
+                if first_loop:
+                    try:
+                        metadata_dict = resp.json()['metadata']
+                        pd_data = pd.DataFrame(resp.json()['data'])
+                    except KeyError:
+                        print(resp.json()['error'])
+                        return None, None
 
-    dates = date_range_string.split('-')
-    assert dates[0] != ''
-    year, month, day = datesToInt(dates[0])
-
-    end_year, end_month, end_day = datesToInt(dates[1])
-    this_is_silly = end_year*10000+end_month*100+end_day
-
-    first_loop = True
-    while 1:
-        end_loop = False
-        
-        if month == 12:
-            next_year += 1
-            next_month = 1
-            next_day = day
-        else:
-            next_year = year
-            next_month = month
-            next_day = day
-        if next_year*10000+next_month*100+end_day < this_is_silly:
-            next_year = end_year
-            next_day = end_day
-            next_month = end_month
-            
-            
-        print('{:02d}/{:02d}/{}'.format(month, day, year))
-        print('{:02d}/{:02d}/{}'.format(next_month, day, next_year))
-        url = 'https://tidesandcurrents.noaa.gov/api/datagetter?'
-        params = {
-            'begin_date': '{:02d}/{:02d}/{}'.format(month, day, year),
-            'end_date':'{:02d}/{:02d}/{}'.format(next_month, day, year),
-            'station':station_id,
-            'product':'water_level',
-            'datum':'mllw',
-            'units':'metric',
-            'time_zone':'gmt',
-            'application':'web_services',
-            'format':'json'    
-        }
-
-            
-        resp = requests.get(url=url, params=params)
-        if first_loop:
-            try:
-                
-                metadata_dict = resp.json()['metadata']
-                pd_data = pd.DataFrame(resp.json()['data'])
-            except KeyError:
-                print(resp.json()['error'])
-                return None, None
-            
-        pd_data.append(resp.json()['data'])
+            pd_data.merge(resp.json()['data'])
         
         month += 1
         if end_loop:
             return metadata_dict, pd_data
-
-
-# In[ ]:
-
-def retrieveRecentData(pandas_thing):
-    station_id = pandas_thing['Station ID']
-
-    url = 'https://tidesandcurrents.noaa.gov/api/datagetter?'
-    params = {
-        'date':'recent',
-        'station':station_id,
-        'product':'currents',
-        'bin':'1',
-        'units':'metric',
-        'time_zone':'gmt',
-        'application':'web_services',
-        'format':'json'    
-    }
-    resp = requests.get(url=url, params=params)
-    try:
-
-        metadata_dict = resp.json()['metadata']
-        pd_data = pd.DataFrame(resp.json()['data'])
-        return metadata_dict, pd_data    
-    
-    except KeyError:
-        print(resp.json()['error'])
-        return None, None
-
-
-
-# In[ ]:
-
-url = 'https://tidesandcurrents.noaa.gov/api/datagetter?'
-params = {
-    'date':'recent',
-    'station':station_id,
-    'product':'currents',
-    'bin':'1',
-    'units':'metric',
-    'time_zone':'gmt',
-    'application':'web_services',
-    'format':'json'    
-}
-resp = requests.get(url=url, params=params)
-try:
-
-    metadata_dict = resp.json()['metadata']
-    pd_data = pd.DataFrame(resp.json()['data'])
-    return metadata_dict, pd_data    
-
-except KeyError:
-    print(resp.json()['error'])
-    return None, None
-
-
-
-# In[ ]:
-
-for index, row in current_stations.iterrows():
-    metadata_dict, pd_data = retrieveRecentData(row)
-    if metadata_dict is not None:
-        break
-    
-
-
-# In[ ]:
-
-with open('token.json') as token_file:
-    header = json.load(token_file)
-
-
-# In[ ]:
-
-try:
-    pd_metadata = json_normalize(resp.json()['metadata'])
-    pd_metadata.columns = ['Station ID', 'Latitude', 'Longitude', 'Location']
-    pd_data = json_normalize(resp.json()['data'])
-    
-except KeyError:
-    print(resp.json()['error'])
-
-
-# In[ ]:
-
-# API for Climate Data
-
-url = 'http://www.ncdc.noaa.gov/cdo-web/api/v2/datatypes?datacategoryid=HYDROMETEOR'
-
-resp = requests.get(url=url, headers=header)
-
-try:
-    pd_metadata = json_normalize(resp.json()['metadata'])
-#     pd_metadata.columns = ['Station ID', 'Latitude', 'Longitude', 'Location']
-    pd_results = json_normalize(resp.json()['results'])
-    
-except KeyError:
-    print(resp.json()['error'])
-    print('Error in the api input')
-
-
-# In[ ]:
-
-url = 'http://www.ncdc.noaa.gov/cdo-web/api/v2/datacategories?limit=41'
-resp = requests.get(url=url, headers=header)
-
-try:
-    pd_metadata = json_normalize(resp.json()['metadata'])
-    # pd_metadata.columns = ['Station ID', 'Latitude', 'Longitude', 'Location']
-    pd_results = json_normalize(resp.json()['results'])
-    
-except KeyError:
-    print(resp.json()['error'])
-    print('Error in the api input')
 
